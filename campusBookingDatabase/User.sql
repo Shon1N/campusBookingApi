fdae586b3cee659ac86a8b69a171e53523061ced
Shoni Nemavhidi <shoni.nemavhidi@typedev.co.za>
2021-07-17 12:12:53 +0200

    Added Api, Database, Models and UI projects

---
+++ campusBookingDatabase/User.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[User]
+(
+	[UserID] INT IDENTITY(1,1) NOT NULL PRIMARY KEY, 
+    [FirstName] VARCHAR(255) NOT NULL, 
+    [LastName] VARCHAR(255) NOT NULL, 
+    [EmailAddress] VARCHAR(255) NOT NULL, 
+    [Password] VARCHAR(255) NOT NULL, 
+    [CellPhoneNumber] VARCHAR(255) NOT NULL
+)
---
+++ campusBookingModel/Models/User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+#nullable disable
+
+namespace campusBookingModel.Models
+{
+    [Table("User")]
+    public partial class User
+    {
+        [Key]
+        [Column("UserID")]
+        public int UserId { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string FirstName { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string LastName { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string EmailAddress { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Password { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string CellPhoneNumber { get; set; }
+    }
+}
